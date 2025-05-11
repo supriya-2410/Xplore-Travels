2a60533b81e465f0e02824f19b116e0b8ba7de82
supriya-2410 <supriyakothai@gmail.com>
2025-05-11 23:58:11 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template
+from urllib.parse import unquote
+import webbrowser 
+import json
+import dbms
+import re
+
+app = Flask(__name__)
+
+@app.route("/home")
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/tag")
+def tag():
+    value = dbms.gettags()
+    data = json.dumps(value)
+    return render_template(template_name_or_list="tag.html",tag=data)
+
+@app.route('/tag/<tag_id>')
+def city_select(tag_id):
+    tag_id = unquote(tag_id)
+    value = dbms.getcitys(tag_id)
+    data = json.dumps(value)
+    return render_template("activity.html",tag=data, t_name=tag_id)
+
+@app.route('/city/<city>')
+def display_city(city):
+    city = unquote(city)
+    value=dbms.get_destination_info(city)
+    data = json.dumps(value)
+    c_name = re.sub(r'\(.*?\)', '', city).strip()
+    value1 = [city.split()[0]+str(x) for x in range(1,5)]
+    data1 = json.dumps(value1)
+    return render_template("city.html", city_slide=data1, city=c_name, city_detail=data)
+
+@app.route("/<click>")
+def other(click):
+    return render_template(template_name_or_list=f"{unquote(click)}.html")
+
+if __name__ == '__main__':  
+   webbrowser.open("http://127.0.0.1:5000/")
+   app.run()
---
+++ static/scripts/activity.js
@@ -0,0 +1,28 @@
+function places(city) {
+    window.location.href = `/city/${city}`;
+}
+
+var data_string = document.getElementById('data').getAttribute('data-value');
+var data_tuple = JSON.parse(data_string);
+
+for (let i = 0; i < data_tuple.length; i++) {
+    var btn1 = document.createElement("button");
+    btn1.className = "button";
+    btn1.id = `${data_tuple[i].split(" ")[0]}`
+    btn1.setAttribute('onclick', `places('${data_tuple[i]}')`);
+    btn1.textContent = `${data_tuple[i]}`;
+    document.body.appendChild(btn1);
+}
+
+const images = document.querySelectorAll('button');
+images.forEach((image) => {
+    // console.log(`/static/activity/${image.id.toLowerCase()}.jpg`)
+    document.getElementById(image.id).style.background =
+        `linear-gradient( rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 1)), url('/static/activity/${image.id.toLowerCase()}.jpg')`
+});
+
+var aBack = document.createElement('a');
+aBack.className = 'back';
+aBack.href = '/tag';
+aBack.innerHTML = 'Home';
+document.body.appendChild(aBack);
